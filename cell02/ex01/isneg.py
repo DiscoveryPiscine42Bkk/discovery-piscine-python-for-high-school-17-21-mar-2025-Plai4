d97ca1bca2b68574a3d1d80068f271bec49e8a47
Plai4 <matepiya64@gmail.com>
2025-03-18 10:35:15 +0700

    ex01

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,21 @@
+a = int(input())
+if a > 0:
+    print("This number is positive.")
+elif a == 0:
+    print("This number is both positive and negative.")
+else:
+    print("This number is negative.")
+b = int(input())
+if b > 0:
+    print("This number is positive.")
+elif b == 0:
+    print("This number is both positive and negative.")
+else:
+    print("This number is negative.")
+c = int(input())
+if c > 0:
+    print("This number is positive.")
+elif c == 0:
+    print("This number is both positive and negative.")
+else:
+    print("This number is negative.")
