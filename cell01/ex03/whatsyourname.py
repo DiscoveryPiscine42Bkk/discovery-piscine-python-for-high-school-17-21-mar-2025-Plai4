00374fa41f14c40e8c1627f82f80b92e7ee566ef
Plai4 <matepiya64@gmail.com>
2025-03-18 10:13:56 +0700

    ex03

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,3 @@
-1 = print(input("Hey, what's your first name?:"))
-2 = print(input("And your last name?:"))
-print("Well, pleased to meet you" + 2 + 3)
+x = input("Hey, what's your first name?:")
+y = input("And your last name?:")
+print("Well, pleased to meet you, " +x,y)
---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,5 @@
+a = int(input("Type any number:"))
+if a == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero")
