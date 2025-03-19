ab5c37b9984581ca41319c329d8518156f505f59
Plai4 <matepiya64@gmail.com>
2025-03-19 11:18:56 +0700

    ex02

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,6 +1,7 @@
-x = int(input("Enter a number less than 25:"))
-while x < 25:
-  x += 1
-  if x == 20:
-    continue
-  print("Inside the loop, my variable is", x)
+num = int(input("Enter a number less than 25:"))
+if num > 25:
+  print("Error")
+else:
+  while num <= 25:
+    print(f"Inside the loop, my variable is {num}.")
+    num += 1
---
+++ cell04/ex03/float.py
@@ -0,0 +1,5 @@
+a = str(input("Give me a number: "))
+if a == float():
+   print("This number is a decimal.")
+else:
+   print("This number is an integer.")
