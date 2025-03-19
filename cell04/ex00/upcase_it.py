28c3a29c467d2030ab8e164b6594a9a0da3f2efa
Plai4 <matepiya64@gmail.com>
2025-03-19 10:10:47 +0700

    ex03

---
+++ cell04/ex00/upcase_it.py
@@ -0,0 +1,3 @@
+txt = str(input("Give me a word:"))
+x = txt.upper()
+print(x)
---
+++ cell04/ex01/age.py
@@ -0,0 +1,5 @@
+x = int(input("Please tell me your age: "))
+print("You are currently",x,"years old.")
+print("In 10 years, you'll be",(x+10),"years old.")
+print("In 20 years, you'll be",(x+20),"years old.")
+print("In 3160 years, you'll be",(x+30),"years old.")
