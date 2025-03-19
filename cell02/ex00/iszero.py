81f3b62d7d1d25d1539e067171f5716fed4f0aea
Plai4 <matepiya64@gmail.com>
2025-03-19 08:54:44 +0700

    ex00

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -2,4 +2,4 @@ a = int(input("Type any number:"))
 if a == 0:
     print("This number is equal to zero.")
 else:
-    print("This number is different from zero")
+    print("This number is different from zero.")
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+while True:
+    user_input = input("What you gotta say?:")
+    if user_input == "STOP":
+        break
+    print(f"I got that! Anything else?:{user_input}")
+
