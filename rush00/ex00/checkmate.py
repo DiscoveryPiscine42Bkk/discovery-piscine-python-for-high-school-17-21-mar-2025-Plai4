d6ca0cd67f98320a9815843e44271fd31b9cab45
Plai4 <matepiya64@gmail.com>
2025-03-21 11:06:53 +0700

    main

--- rush00/ex00/checkmate.py
+++ rush00/ex00/checkmate.py
@@ -1,17 +1,22 @@
-# check king valid moves
-def check_king(position, color):
-    moves_list = []
-    if color == 'white':
-        enemies_list = black_locations
-        friends_list = white_locations
-    else:
-        friends_list = black_locations
-        enemies_list = white_locations
-    # 8 squares to check for kings, they can go one square any direction
-    targets = [(1, 0), (1, 1), (1, -1), (-1, 0),
-               (-1, 1), (-1, -1), (0, 1), (0, -1)]
-    for i in range(8):
-        target = (position[0] + targets[i][0], position[1] + targets[i][1])
-        if target not in friends_list and 0 <= target[0] <= 7 and 0 <= target[1] <= 7:
-            moves_list.append(target)
-    return moves_list
+class GameState:
+    # other methods / properties
+
+    @property
+    def legal_moves(self) -> Iterable[Move]: 
+        """Finds all legal Moves for the current Player (also considers check)"""
+        # TODO: implement
+
+    @property
+    def is_check(self) -> bool:
+        """Checks if the last move was a check (or checkmate)"""
+        # TODO: implement
+
+    @property
+    def is_checkmate(self) -> bool:
+        """Checks if the last move was a checkmate"""
+
+        if not self.is_check:
+            return False  # If the last move was not a check, it also wasn't a checkmate
+        for move in self.legal_moves():
+            return False  # There exists at least one move that gets the player out of check
+        return True  # No move gets the player get out of check, so therefore they are checkmate
